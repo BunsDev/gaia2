18b3210b3bec79b35255ad2e7348cc200bfa245b
Nathaniel Yeboah <yeboahnat99@gmail.com>
2024-06-02 22:15:51 +0000

    feat: Update target network to sepolia in scaffold.config.ts
    This commit updates the target network in the `scaffold.config.ts` file from `chains.foundry` to `chains.sepolia`. This change ensures that the DApp is now live on the sepolia network.
    
    Note: This commit message follows the established convention of using a prefix (`feat`) to indicate a new feature or enhancement.

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -3,33 +3,46 @@ pragma solidity ^0.8.19;
 
 import "../contracts/YourContract.sol";
 import "./DeployHelpers.s.sol";
-
-import {Deployerment} from "./scriptsdeployment/Deployerment.s.sol";
-import {DefiMarketPlace} from "../contracts/DefiMarketPlace.sol";
+import {SportUpdate} from "../contracts/SportUpdate.sol";
+import {HelperConfig} from "./HelperConfig.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
 
+    struct NetworkConfig {
+        address functionsRouter;
+        bytes32 donId;
+        uint64 subId;
+    }
+
+    address public functionsRouter;
+    bytes32 public donId;
+    uint64 public subId;
+    string public fixtureSource;
+    string public mactchSource;
+    uint32 constant CALLBACK_GAS_LIMIT = 3000000;
+
+    HelperConfig public config;
+    NetworkConfig activeNetwork;
+
+    string constant jsfixtureSource = "./functions/sources/fixature.js";
+    string constant jsmactchSource = "./functions/sources/match.js";
+
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
+
+        getConfigVariable();
+
         if (deployerPrivateKey == 0) {
             revert InvalidPrivateKey(
                 "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
             );
         }
-
-        // deploy the written contracts
-        Deployerment deployerment = new Deployerment();
-
         vm.startBroadcast(deployerPrivateKey);
-        // YourContract yourContract = new YourContract(vm.addr(deployerPrivateKey));
-        // console.logString(string.concat("YourContract deployed at: ", vm.toString(address(yourContract))));
 
-        // DefiMarketPlace deploerment
-        address ethAddress = deployerment.getEthUsdPriceFeed();
-        // DefiMarketPlace marketPlace = new DefiMarketPlace(ethAddress);
-        // DefiMarketPlace marketPlace = deployerment.marketPlace();
-        // console.logString(string.concat("DefiMarketPlace deployed at: ", vm.toString(address(marketPlace))));
+        SportUpdate sportUpdate =
+            new SportUpdate(fixtureSource, mactchSource, functionsRouter, donId, subId, CALLBACK_GAS_LIMIT);
+
         vm.stopBroadcast();
 
         /**
@@ -38,6 +51,13 @@ contract DeployScript is ScaffoldETHDeploy {
          * This function should be called last.
          */
         exportDeployments();
+    }
+
+    function getConfigVariable() public {
+        config = new HelperConfig();
+        (functionsRouter, donId, subId) = config.activeNetworkConfig();
+        fixtureSource = vm.readFile(jsfixtureSource);
+        mactchSource = vm.readFile(jsmactchSource);
     }
 
     function test() public {}
